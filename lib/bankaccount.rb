fe168d8bf0c453b8c93619103c7da4e0ee8cf301
Muhammad Subhan <subhanansari586@gmail.com>
2019-12-04 17:35:27 +0000

    First three user stories

--- lib/bankaccount.rb
+++ lib/bankaccount.rb
@@ -1,10 +1,15 @@
 class BankAccount
   attr_reader :balance
-  
-  def initialize
+
+  def initialize()
     @balance = 0
   end
+
   def deposit(money)
     @balance += money
+  end
+
+  def withdraw(money)
+    @balance -= money
   end
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -7,5 +7,11 @@ describe BankAccount do
     expect(bank.balance).to eq 5
   end
 
+it 'can withdraw money from the bank account' do
+  bank = BankAccount.new
+  #bank.deposit(5)
+  bank.withdraw(2)
+  expect(bank.balance).to eq (- 2)
+end
 
 end
